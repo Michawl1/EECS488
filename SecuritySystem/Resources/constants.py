225c936442c5271095cf73442606466a7161853a
Michael Thompson <mjt106@case.edu>
2020-04-23 15:39:39 -0400

    restructured again

---
+++ SecuritySystem/Resources/constants.py
@@ -0,0 +1,18 @@
+"""
+Constants for the use of the application
+"""
+LowRes = {
+    "width": 853,
+    "height": 480,
+    "fps": 3
+}
+MedRes = {
+    "width": 1280,
+    "height": 720,
+    "fps": 6
+}
+HighRes = {
+    "width": 1920,
+    "height": 1080,
+    "fps": 10
+}
